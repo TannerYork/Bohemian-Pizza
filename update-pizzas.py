2be0519a35e552b9f4415673fd7258effb5eb583
Tanner York <tannerwyork@gmail.com>
2019-10-07 16:52:25 -0700

    Add script for updating mongodb store items

---
+++ update-pizzas.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import json
+
+client = MongoClient()
+db = client.PizzaStore
+pizzas = db.pizzas
+pizzas.delete_many({})
+
+pizzas_file = open('scripts/pizzas.txt', 'r')
+pizza_documents = pizzas_file.readlines()
+for line in pizza_documents:
+    pizza_json = json.loads(line)
+    name = pizza_json["name"]
+    if pizza_json: pizzas.insert_one(pizza_json)
+    else: print(f"Error adding {name} check for syntax error")
+pizzas_file.close()
