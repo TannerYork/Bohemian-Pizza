6094793fd5aecf92c4ac4c6d44e14ba56fb66ee4
Tanner York <tannerwyork@gmail.com>
2019-10-08 19:22:34 -0700

    Add user login, logout, and regisure functionality

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import bcrypt
 from datetime import datetime
 import os
 
 client = MongoClient()
 db = client.PizzaStore
 pizzas = db.pizzas
+users = db.users
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY')
 
@@ -14,7 +16,9 @@ app.secret_key = os.environ.get('SECRET_KEY')
 @app.route('/')
 def pizzas_index():
     '''Renders the landing page'''
-    if 'anonymous-cart' not in session:
+    if 'user' in session:
+        return render_template('index.html', user=session['user'])
+    elif 'anonymous-cart' not in session:
         session['anonymous-cart'] = {}
     return render_template('index.html')
 
@@ -34,7 +38,6 @@ def pizzas_add():
     '''Uploads pizzas\' _id to users cart'''
     if 'anonymous-cart' in session:
         cart = session['anonymous-cart']
-        print(cart)
         pizza_id = str(request.form.get('_id'))
         quantity = int(request.form.get('quantity'))
         if pizza_id in cart:
@@ -42,7 +45,7 @@ def pizzas_add():
         else:
             cart.update({pizza_id: quantity})
         session['anonymous-cart'] = cart
-    return redirect(url_for('pizzas_shop', pizzas=pizzas.find()))
+    return redirect(url_for('pizzas_shop'))
 
 @app.route('/update_cart', methods=['POST'])
 def pizzas_update_cart():
@@ -55,8 +58,51 @@ def pizzas_update_cart():
             pizza = { request.form.get('_id'): int(request.form.get('quantity')) }
             session_cart.update(pizza)
         session['anonymous-cart'] = session_cart
-        document_cart = get_cart()
-    return redirect(url_for('pizzas_cart', pizzas=document_cart))
+    return redirect(url_for('pizzas_cart'))
+
+@app.route('/login')
+def pizzas_login():
+    '''Render the login page'''
+    if 'user' in session: return render_template('pizzas_login.html', user=True)
+    else: return render_template('pizzas_login.html')
+    
+@app.route('/user-login', methods=["POST"])
+def user_login():
+    '''Login user if password is correct'''
+    user = users.find_one({'email': request.form.get('email')})
+    encoded_pass = request.form.get('password').encode('utf-8')
+    if user and bcrypt.checkpw(encoded_pass, user['password']):
+        session.clear()
+        session['user'] = { 'display_name': user['display_name'], 'email': user['email']}
+        return redirect(url_for('pizzas_index'))
+    return render_template('pizzas_login.html', error=True)
+
+@app.route('/user-logout', methods=["POST"])
+def user_logout():
+    '''Logout user'''
+    if 'user' in session: del session['user']
+    return redirect(url_for('pizzas_index'))
+    
+@app.route('/registure')
+def pizzas_registure():
+    if 'user' in session: return render_template('pizzas_registure.html', user=True)
+    else: return render_template('pizzas_registure.html')
+
+@app.route('/user-registure', methods=["POST"])
+def user_registure():
+    '''Add a new user to mongodb database'''
+    encoded_pass = request.form.get('password').encode('utf-8')
+    user = {
+        'display_name': request.form.get('display_name'),
+        'email': request.form.get('email'),
+        'password': bcrypt.hashpw(encoded_pass, bcrypt.gensalt())
+    }
+    user_exist = users.find_one({'email': user['email']})
+    if user_exist:
+        return render_template('pizzas_registure.html', user_exist=True)
+    else:
+        users.insert_one(user)
+        return redirect(url_for('pizzas_login'))
 
 def get_cart():
     '''A helper function for getting the user's cart from cookies or mongodb'''
