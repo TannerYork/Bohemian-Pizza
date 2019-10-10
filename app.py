747560ec238e2b78c79145e8c4ca2afcd2bf2ace
Tanner York <tannerwyork@gmail.com>
2019-10-10 08:39:57 -0700

    Fix heroku load-documents crash error

--- app.py
+++ app.py
@@ -17,17 +17,6 @@ comments = db.comments
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY')
 
-pizzas.delete_many({})
-
-pizzas_file = open('pizzas.txt', 'r')
-pizza_documents = pizzas_file.readlines()
-for line in pizza_documents:
-    pizza_json = json.loads(line)
-    name = pizza_json["name"]
-    if pizza_json: pizzas.insert_one(pizza_json)
-    else: print(f"Error adding {name} check for syntax error")
-pizzas_file.close()
-
 
 @app.route('/')
 def pizzas_index():
